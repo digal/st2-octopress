f8947ebb3c221464964a898cdae197efd1576b89
Yuri Buyanov <yuri.buyanov@gmail.com>
2011-09-27 10:20:49 +0400

    added basic package files

---
+++ README.md
@@ -0,0 +1,22 @@
+## Octopress plugin for Sublime Text 2
+
+Note: Plugin assumes that you have your octopress folder opened in the file pane. For now, 'octopress folder' means 'first folder with Rakefile in it' :) 
+
+## Installation 
+
+Drop repo contents to `Packages/Octopress` directory. Normal Sublime package coming soon.
+
+## Commands supported
+
+- `octo_post` ("Octopress: New post" from the command palette) - create new post (ask for title)
+
+## TODO:
+
+- Proper packaging
+- Preview
+- Deploy
+
+## Links
+
+[Sublime text 2](http://www.sublimetext.com/2)
+[Octopress](http://octopress.org/)
--- octopress.py
+++ octopress.py
@@ -1,41 +1,45 @@
 import sublime, sublime_plugin, glob, os, subprocess, time
 
-class PostCommand(sublime_plugin.TextCommand):
-  
-  def run(self, edit):
-    print("hello?")
-    octopress_dir = self.find_octopress_dir().next()
-    print("Octopress dir: %s" % octopress_dir)
-    self.new_post_in(octopress_dir)
-    #except:
-    #  sublime.error_message(u'Cannot find octopress dir!')
-        
-      
-    #self.view.insert(edit, 0, "Hello, World!")
+class OctoPostCommand(sublime_plugin.WindowCommand):
 
-  def done(self, text):
+  def description(self):
+    return "Creates a new octopress post"
+  
+  def run(self):
+    print("hello?")
+    try:
+      octopress_dir = self.find_octopress_dir().next()
+      print("Octopress dir: %s" % octopress_dir)
+      self.new_post_in(octopress_dir)
+    except:
+      sublime.error_message(u'Cannot find octopress dir!')
+      
+  def new_post_done(self, text):
     result = subprocess.Popen(
       'rake new_post["%s"]' % text, 
-      shell = True, 
+      shell  = True, 
       stdout = subprocess.PIPE, 
-      cwd = self.octopress_dir
+      stderr = subprocess.STDOUT, 
+      cwd    = self.octopress_dir
     ).communicate()[0]
     print(result)
     filename = os.path.join(self.octopress_dir, result.replace('Creating new post: ', '').replace('\n', ''))
     print("Opening %s" % filename)
-    self.view.window().open_file(filename)
+    self.window.open_file(filename)
 
 
   ##TODO: improve octopress dir criteria  
   def find_octopress_dir(self):
-    for directory in self.view.window().folders():
+    for directory in self.window.folders():
       print(os.path.join(directory, u'Rakefile'))
       if (len(glob.glob(os.path.join(directory, u'Rakefile'))) > 0):
         yield directory
 
   def new_post_in(self, octopress_dir):
     self.octopress_dir = octopress_dir
-    self.view.window().show_input_panel("Post title", "", self.done, None, None)
+    self.window.show_input_panel("Post title", "", self.new_post_done, None, None)
+
+  
     
 
       
