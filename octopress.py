a32d3a43a571d1563611355b532c29c77d880293
Yuri Buyanov <yuri.buyanov@gmail.com>
2011-09-28 22:24:02 +0400

    small refactoring, deploy cmd (yet to be improved)

--- octopress.py
+++ octopress.py
@@ -1,17 +1,29 @@
 import sublime, sublime_plugin, glob, os, subprocess, time
 
-class OctoPostCommand(sublime_plugin.WindowCommand):
+
+class OctoCommandBase(sublime_plugin.WindowCommand):
+
+  ##TODO: improve octopress dir criteria  
+  def find_octopress_dir(self):
+    for directory in self.window.folders():
+      print(os.path.join(directory, u'Rakefile'))
+      if glob.glob(os.path.join(directory, u'Rakefile')):
+        return directory
+    return None
+
+
+class OctoPostCommand(OctoCommandBase):
 
   def description(self):
     return "Creates a new octopress post"
   
   def run(self):
     print("hello?")
-    try:
-      octopress_dir = self.find_octopress_dir().next()
+    octopress_dir = self.find_octopress_dir()
+    if octopress_dir != None:
       print("Octopress dir: %s" % octopress_dir)
       self.new_post_in(octopress_dir)
-    except:
+    else:
       sublime.error_message(u'Cannot find octopress dir!')
       
   def new_post_done(self, text):
@@ -27,17 +39,35 @@ class OctoPostCommand(sublime_plugin.WindowCommand):
     print("Opening %s" % filename)
     self.window.open_file(filename)
 
-
-  ##TODO: improve octopress dir criteria  
-  def find_octopress_dir(self):
-    for directory in self.window.folders():
-      print(os.path.join(directory, u'Rakefile'))
-      if glob.glob(os.path.join(directory, u'Rakefile')):
-        yield directory
-
   def new_post_in(self, octopress_dir):
     self.octopress_dir = octopress_dir
     self.window.show_input_panel("Post title", "", self.new_post_done, None, None)
+
+
+class OctoDeployCommand(OctoCommandBase):
+
+  def description(self):
+    return "Deploys blog"
+
+  def run(self):
+    print("hello?")
+    octopress_dir = self.find_octopress_dir()
+    if octopress_dir != None:
+      print("Octopress dir: %s" % octopress_dir)
+      self.deploy_in(octopress_dir)
+    else:
+      sublime.error_message(u'Cannot find octopress dir!')
+
+  def deploy_in(self, octopress_dir):
+    result = subprocess.Popen(
+      'rake deploy', 
+      shell  = True, 
+      stdout = subprocess.PIPE, 
+      stderr = subprocess.STDOUT, 
+      cwd    = octopress_dir
+    ).communicate()[0]
+    print(result)
+    sublime.status_message(result)
 
   
     
